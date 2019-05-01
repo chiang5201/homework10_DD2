4d35c3b850a2724ea3a69bfc193cd4891e4ffdeb
chiang5201 <rocky9@vt.edu>
2019-05-01 01:37:38 -0400

    resolved merge conflicts.

--- XGA_Text.v
+++ XGA_Text.v
@@ -326,15 +326,15 @@ module chat(input  wire clk,
 				output wire  [6:0]out);
 reg [6:0] display, nextdisplay;
 
-//wire shift;
-//assign shift = 8'b0001_0010;
-
+				input  wire reset,
+				input  wire [7:0]in,
+				output reg [6:0]out);
+reg [6:0] display;
 
 reg shift, nextshift;
 
 
-always @(posedge clk)
-begin
+always @(posedge clk)          begin
 	if(reset) begin
 		shift <= 0;
 		display <= 0;
@@ -343,6 +343,7 @@ begin
 		display <= nextdisplay;
 	end
 end
+	
 
 always @(*) begin
 nextdisplay = display;
@@ -350,34 +351,71 @@ nextshift = shift;
 
 if(in==8'b0001_0010 || in==8'h59) nextshift = ~shift;
 
-if(shift)
+if(~shift)
 	case (in)
-8'b0100_0101:nextdisplay=7'h30;//"0"
-8'b0001_0110:nextdisplay=7'h31;//"1"
-8'b0001_1110:nextdisplay=7'h32;//"2"
-8'b0010_0110:nextdisplay=7'h33;//"3"
-8'b0010_0101:nextdisplay=7'h34;//"4"
-8'b0010_1110:nextdisplay=7'h35;//"5"
-8'b0011_0110:nextdisplay=7'h36;//"6"
-8'b0011_1101:nextdisplay=7'h37;//"7"
-8'b0011_1110:nextdisplay=7'h38;//"8"
-8'b0100_0110:nextdisplay=7'h39;//"9"
-default:nextdisplay=display;
-endcase
+	8'b0100_0101:display=7'h30;//"0"
+	8'b0001_0110:display=7'h31;//"1"
+	8'b0001_1110:display=7'h32;//"2"
+	8'b0010_0110:display=7'h33;//"3"
+	8'b0010_0101:display=7'h34;//"4"
+	8'b0010_1110:display=7'h35;//"5"
+	8'b0011_0110:display=7'h36;//"6"
+	8'b0011_1101:display=7'h37;//"7"
+	8'b0011_1110:display=7'h38;//"8"
+	8'b0100_0110:display=7'h39;//"9"
+   8'h52: display = 8'h27; //'
+   8'h41: display = 8'h2C; //,
+   8'h4E: display = 8'h2D; //-
+   8'h49: display = 8'h2E; //.
+   8'h4A: display = 8'h2F; ///
+   8'h4C: display = 8'h3B; //;
+   8'h55: display = 8'h3D; //=
+   8'h54: display = 8'h5B; //[
+   8'h5D: display = 8'h5C; //\
+   8'h5B: display = 8'h5D; //]
+   8'h0E: display = 8'h60; //`  	
+	8'h1C: display = 8'h61; //a
+   8'h32: display = 8'h62; //b
+   8'h21: display = 8'h63; //c
+   8'h23: display = 8'h64; //d
+   8'h24: display = 8'h65; //e
+   8'h2B: display = 8'h66; //f
+   8'h34: display = 8'h67; //g
+   8'h33: display = 8'h68; //h
+   8'h43: display = 8'h69; //i
+   8'h3B: display = 8'h6A; //j
+   8'h42: display = 8'h6B; //k
+   8'h4B: display = 8'h6C; //l
+   8'h3A: display = 8'h6D; //m
+   8'h31: display = 8'h6E; //n
+   8'h44: display = 8'h6F; //o
+   8'h4D: display = 8'h70; //p
+   8'h15: display = 8'h71; //q
+   8'h2D: display = 8'h72; //r
+   8'h1B: display = 8'h73; //s
+   8'h2C: display = 8'h74; //t
+   8'h3C: display = 8'h75; //u
+   8'h2A: display = 8'h76; //v
+   8'h1D: display = 8'h77; //w
+   8'h22: display = 8'h78; //x
+   8'h35: display = 8'h79; //y
+   8'h1A: display = 8'h7A; //z	
+	default:display=7'h00;
+	endcase
 
 else
-case(in)
-8'b0100_0101:nextdisplay=7'h20;//")"
-8'b0001_0110:nextdisplay=7'h21;//"1"
-8'b0001_1110:nextdisplay=7'h40;//"2"
-8'b0010_0110:nextdisplay=7'h23;//"3"
-8'b0010_0101:nextdisplay=7'h24;//"4"
-8'b0010_1110:nextdisplay=7'h25;//"5"
-8'b0011_0110:nextdisplay=7'h26;//"6"
-8'b0011_1101:nextdisplay=7'h27;//"7"
-8'b0011_1110:nextdisplay=7'h28;//"8"
-8'b0100_0110:nextdisplay=7'h29;//"9"
-default:nextdisplay=display;
+	case(in)
+   8'b0100_0101:display=7'h20;//")"
+	8'b0001_0110:display=7'h21;//"!"
+	8'b0001_1110:display=7'h40;//"@"
+	8'b0010_0110:display=7'h23;//"#"
+	8'b0010_0101:display=7'h24;//"$"
+	8'b0010_1110:display=7'h25;//"%"
+	8'b0011_0110:display=7'h26;//"^"
+	8'b0011_1101:display=7'h27;//"&"
+	8'b0011_1110:display=7'h28;//"*"
+	8'b0100_0110:display=7'h29;//"("
+	default:display=7'h00;
 	endcase
 
 end 
